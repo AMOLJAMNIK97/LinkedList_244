800ca287e0e279e5cd18f880d074c2b52c8e908a
Amol <ajamnik.mech@gmail.com>
2023-01-17 17:19:49 +0530

    [Amol]- Create a Simple Linked List

---
+++ LinkedList_Data/LinkedList_Data/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Data
+{
+    public class Node
+    {
+        public int data;
+        public Node Next;
+        public Node(int data)//perimetrized constructor
+        {
+            this.data = data;
+            Next = null;
+        }
+    }
+}
---
+++ LinkedList_Data/LinkedList_Data/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Addnote(56);
+            list.Addnote(30);
+            list.Addnote(70);
+            list.Display();
+
+        }
+    }
+}
