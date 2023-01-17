e5d0cd3e8fe8416c38453dad12f5055950393aa7
Amol <ajamnik.mech@gmail.com>
2023-01-17 20:56:17 +0530

    [Amol]- Create Linked List by appending

--- LinkedList_Data/LinkedList_Data/Program.cs
+++ LinkedList_Data/LinkedList_Data/Program.cs
@@ -11,16 +11,16 @@ namespace LinkedList_Data
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            list.Addnote(56);
-            list.Addnote(30);
-            list.Addnote(70);
-            list.Display();
-
-
-            //list.Addreverse(70);
-            //list.Addreverse(30);
-            //list.Addreverse(56);
+            //list.Addnote(56);
+            //list.Addnote(30);
+            //list.Addnote(70);
             //list.Display();
+
+
+            list.Addreverse(70);
+            list.Addreverse(30);
+            list.Addreverse(56);
+            list.Display();
 
 
         }
