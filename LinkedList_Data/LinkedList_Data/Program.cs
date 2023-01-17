364f29f53ec61c3d3edc4e9c7eef4bd0cb6c2edf
Amol <ajamnik.mech@gmail.com>
2023-01-17 21:22:22 +0530

    [Amol]- Ability to insert

--- LinkedList_Data/LinkedList_Data/Program.cs
+++ LinkedList_Data/LinkedList_Data/Program.cs
@@ -11,15 +11,22 @@ namespace LinkedList_Data
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            //list.Addnote(56);
-            //list.Addnote(30);
-            //list.Addnote(70);
+            list.Addnote(56);
+            list.Addnote(30);
+            list.Addnote(70);
             //list.Display();
 
 
-            list.Addreverse(70);
-            list.Addreverse(30);
-            list.Addreverse(56);
+            //list.Addreverse(70);
+            //list.Addreverse(30);
+            //list.Addreverse(56);
+            //list.Display();
+
+            list.Insert(2, 30);
+            list.Insert(2, 56);
+            //list.Insert(2, 56);
+            
+            
             list.Display();
 
 
